5504cc07d0f6c5521c5a57e62959ff2d88fb2e9f
twenk11k <selim_demiral@outlook.com>
2021-12-04 16:40:32 +0300

    Add NetworkModule to initialize OkHttpClient

---
+++ app/src/main/java/com/twenk11k/tasksearch/di/NetworkModule.kt
@@ -0,0 +1,21 @@
+package com.twenk11k.tasksearch.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+}
