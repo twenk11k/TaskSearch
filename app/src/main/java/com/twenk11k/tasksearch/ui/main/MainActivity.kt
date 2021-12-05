e7f5834007ea531b884c4b2e8e7444268bc2f26d
twenk11k <selim_demiral@outlook.com>
2021-12-06 00:55:54 +0300

    Use AndroidEntryPoint annotation to setup hilt in MainActivity

--- app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
@@ -5,7 +5,9 @@ import android.view.Menu
 import com.twenk11k.tasksearch.R
 import com.twenk11k.tasksearch.binding.DataBindingActivity
 import com.twenk11k.tasksearch.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : DataBindingActivity() {
 
     private val binding: ActivityMainBinding by binding(R.layout.activity_main)
