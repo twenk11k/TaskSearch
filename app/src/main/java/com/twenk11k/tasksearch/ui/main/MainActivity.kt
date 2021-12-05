335987d0651d97ef010a1e6813733762bd69bdb0
twenk11k <selim_demiral@outlook.com>
2021-12-05 15:15:53 +0300

    Inflate menu_search menu in MainActivity

--- app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.twenk11k.tasksearch.ui.main
 
 import android.os.Bundle
+import android.view.Menu
 import com.twenk11k.tasksearch.R
 import com.twenk11k.tasksearch.binding.DataBindingActivity
 import com.twenk11k.tasksearch.databinding.ActivityMainBinding
@@ -14,6 +15,11 @@ class MainActivity : DataBindingActivity() {
         binding.apply {
             lifecycleOwner = this@MainActivity
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        return true
     }
 
 }
