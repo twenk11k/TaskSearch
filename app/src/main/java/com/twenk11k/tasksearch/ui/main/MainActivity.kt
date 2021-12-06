249dee50614b924aac26e00c7ffba9fcf97118e3
twenk11k <selim_demiral@outlook.com>
2021-12-06 20:48:55 +0300

    Apply MainViewModel to activity_main.xml via data binding

--- app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.twenk11k.tasksearch.ui.main
 
 import android.os.Bundle
 import android.view.Menu
+import androidx.activity.viewModels
 import com.twenk11k.tasksearch.R
 import com.twenk11k.tasksearch.binding.DataBindingActivity
 import com.twenk11k.tasksearch.databinding.ActivityMainBinding
@@ -11,11 +12,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : DataBindingActivity() {
 
     private val binding: ActivityMainBinding by binding(R.layout.activity_main)
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.apply {
             lifecycleOwner = this@MainActivity
+            vm = mainViewModel
         }
     }
 
