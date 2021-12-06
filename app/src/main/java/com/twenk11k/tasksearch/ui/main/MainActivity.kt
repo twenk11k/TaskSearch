9bf3ddcf2bb803092dd3b922ae910e093cc6b3d1
twenk11k <selim_demiral@outlook.com>
2021-12-06 21:12:53 +0300

    Add handleSearch method to call setQuery method of MainViewModel whenever the text query changes

--- app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/twenk11k/tasksearch/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.twenk11k.tasksearch.ui.main
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
 import com.twenk11k.tasksearch.R
 import com.twenk11k.tasksearch.binding.DataBindingActivity
 import com.twenk11k.tasksearch.databinding.ActivityMainBinding
@@ -25,7 +28,25 @@ class MainActivity : DataBindingActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_search, menu)
+        handleSearch(menu)
         return true
+    }
+
+    private fun handleSearch(menu: Menu) {
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    mainViewModel.setQuery(newText!!)
+                    return true
+                }
+            })
+        }
     }
 
 }
