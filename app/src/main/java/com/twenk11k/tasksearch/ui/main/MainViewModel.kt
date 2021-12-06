0c99aaaa27b74e5b496ae97407a1aa52479a0640
twenk11k <selim_demiral@outlook.com>
2021-12-06 21:00:42 +0300

    Handle response from API in MainViewModel

--- app/src/main/java/com/twenk11k/tasksearch/ui/main/MainViewModel.kt
+++ app/src/main/java/com/twenk11k/tasksearch/ui/main/MainViewModel.kt
@@ -1,9 +1,32 @@
 package com.twenk11k.tasksearch.ui.main
 
-import androidx.lifecycle.ViewModel
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableField
+import androidx.lifecycle.*
 import com.twenk11k.tasksearch.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel()
+class MainViewModel @Inject constructor(private val mainRepository: MainRepository) : ViewModel() {
+
+    var taskListLiveData: LiveData<List<Any>?>
+    val isLoading = ObservableBoolean(false)
+    val toastMessage = ObservableField<String>()
+
+    private val query = MutableStateFlow("")
+
+    init {
+        taskListLiveData = query.asLiveData().switchMap {
+            mainRepository.getTaskSearch(
+                query = query.value,
+                onStart = { isLoading.set(true) },
+                onComplete = { isLoading.set(false) },
+                onError = { toastMessage.set(it) }
+            ).asLiveData(viewModelScope.coroutineContext + Dispatchers.IO)
+        }
+    }
+
+}
