d42ff78dfba1f46a5bf1a3ea0d3138e6fc9bc50c
twenk11k <selim_demiral@outlook.com>
2021-12-06 20:49:41 +0300

    Add TaskAdapter

---
+++ app/src/main/java/com/twenk11k/tasksearch/ui/main/TaskAdapter.kt
@@ -0,0 +1,41 @@
+package com.twenk11k.tasksearch.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.twenk11k.tasksearch.R
+import com.twenk11k.tasksearch.data.model.TaskItem
+import com.twenk11k.tasksearch.databinding.ItemTaskBinding
+
+class TaskAdapter : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+
+    private val items = ArrayList<Any>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemTaskBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_task,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.apply {
+            task = items[position] as TaskItem
+        }
+    }
+
+    fun updateAdapter(list: List<Any>) {
+        items.clear()
+        items.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root)
+
+}
