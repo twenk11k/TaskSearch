92d465f2183306c3bb4d8e25f36a82e0f265ed54
twenk11k <selim_demiral@outlook.com>
2021-12-06 20:45:39 +0300

    Add TaskItem and TaskSearchRequest models

---
+++ app/src/main/java/com/twenk11k/tasksearch/data/model/TaskItem.kt
@@ -0,0 +1,3 @@
+package com.twenk11k.tasksearch.data.model
+
+data class TaskItem(val name: String, val project: String)
---
+++ app/src/main/java/com/twenk11k/tasksearch/data/model/TaskSearchRequest.kt
@@ -0,0 +1,3 @@
+package com.twenk11k.tasksearch.data.model
+
+data class TaskSearchRequest(val text: String)
