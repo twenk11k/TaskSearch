9c1285225a39a17e1e8421c776469c6f0e9b8204
twenk11k <selim_demiral@outlook.com>
2021-12-08 07:57:44 +0300

    Call setStatus from onTabSelected method with 400ms delay

--- app/src/main/java/com/twenk11k/tasksearch/binding/ViewBinding.kt
+++ app/src/main/java/com/twenk11k/tasksearch/binding/ViewBinding.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import com.google.android.material.tabs.TabLayout
 import com.twenk11k.tasksearch.ui.main.MainViewModel
+import com.twenk11k.tasksearch.util.Constants.SEARCH_DELAY
 
 object ViewBinding {
 
@@ -31,11 +32,13 @@ object ViewBinding {
     fun bindTabSelected(tabLayout: TabLayout, viewModel: MainViewModel) {
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab?.position) {
-                    0 -> viewModel.setStatus(0)
-                    1 -> viewModel.setStatus(1)
-                    2 -> viewModel.setStatus(18)
-                }
+                tabLayout.postDelayed({
+                    when (tab?.position) {
+                        0 -> viewModel.setStatus(0)
+                        1 -> viewModel.setStatus(1)
+                        2 -> viewModel.setStatus(18)
+                    }
+                }, SEARCH_DELAY)
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
