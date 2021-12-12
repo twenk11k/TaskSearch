839b67a75df53594ce66151e8edbaf2e86d59f4f
twenk11k <selim_demiral@outlook.com>
2021-12-12 17:43:56 +0300

    Add Entity annotation to TaskItem

--- app/src/main/java/com/twenk11k/tasksearch/data/model/TaskItem.kt
+++ app/src/main/java/com/twenk11k/tasksearch/data/model/TaskItem.kt
@@ -1,9 +1,11 @@
 package com.twenk11k.tasksearch.data.model
 
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "tasks")
 data class TaskItem(
     val id: Int,
     val name: String,
