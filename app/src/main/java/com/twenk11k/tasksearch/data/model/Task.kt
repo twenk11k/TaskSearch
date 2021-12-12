3a47e2ae7f9fdb226690589ee98d9d41d0c437bb
twenk11k <selim_demiral@outlook.com>
2021-12-12 17:50:33 +0300

    Add id and status parameters to Task model

--- app/src/main/java/com/twenk11k/tasksearch/data/model/Task.kt
+++ app/src/main/java/com/twenk11k/tasksearch/data/model/Task.kt
@@ -3,7 +3,9 @@ package com.twenk11k.tasksearch.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Task(
+    val id: Int,
     val name: String,
+    val status: Int,
     @SerializedName("section_id")
     val sectionId: Int
 )
