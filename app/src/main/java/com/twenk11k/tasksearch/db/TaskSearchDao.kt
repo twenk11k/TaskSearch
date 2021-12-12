d418e475b4fcba1e058e41b8e41f2e39074ec11e
twenk11k <selim_demiral@outlook.com>
2021-12-12 17:48:10 +0300

    Add insertTaskItemList method to TaskSearchDao

--- app/src/main/java/com/twenk11k/tasksearch/db/TaskSearchDao.kt
+++ app/src/main/java/com/twenk11k/tasksearch/db/TaskSearchDao.kt
@@ -1,6 +1,14 @@
 package com.twenk11k.tasksearch.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.twenk11k.tasksearch.data.model.TaskItem
 
 @Dao
-interface TaskSearchDao
+interface TaskSearchDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTaskItemList(taskItemList: List<TaskItem>)
+
+}
