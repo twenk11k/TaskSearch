ebba9355177be28ef063219a76e639adde361b68
twenk11k <selim_demiral@outlook.com>
2021-12-12 18:17:45 +0300

    Use BEARER const from Constants in HttpRequestInterceptor

--- app/src/main/java/com/twenk11k/tasksearch/data/network/HttpRequestInterceptor.kt
+++ app/src/main/java/com/twenk11k/tasksearch/data/network/HttpRequestInterceptor.kt
@@ -1,5 +1,6 @@
 package com.twenk11k.tasksearch.data.network
 
+import com.twenk11k.tasksearch.util.Constants.BEARER
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -8,7 +9,7 @@ class HttpRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         request = request.newBuilder()
-            .header("Authorization", "Bearer 2N6edq_uS3ACq89RhzN2yQtdT5aEhbKgaE5-P9BD3hc").build()
+            .header("Authorization", "Bearer $BEARER").build()
         return chain.proceed(request)
     }
 
