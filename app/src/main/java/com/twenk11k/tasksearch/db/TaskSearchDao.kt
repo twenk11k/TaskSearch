e480d6a49d8c99f3f4cd8d4aa0d4d440296a1618
twenk11k <selim_demiral@outlook.com>
2021-12-12 17:48:46 +0300

    Add getTaskItemList method to TaskSearchDao

--- app/src/main/java/com/twenk11k/tasksearch/db/TaskSearchDao.kt
+++ app/src/main/java/com/twenk11k/tasksearch/db/TaskSearchDao.kt
@@ -3,6 +3,7 @@ package com.twenk11k.tasksearch.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.twenk11k.tasksearch.data.model.TaskItem
 
 @Dao
@@ -10,5 +11,8 @@ interface TaskSearchDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTaskItemList(taskItemList: List<TaskItem>)
+
+    @Query("SELECT * FROM tasks WHERE name LIKE :query")
+    suspend fun getTaskItemList(query: String): List<TaskItem>
 
 }
