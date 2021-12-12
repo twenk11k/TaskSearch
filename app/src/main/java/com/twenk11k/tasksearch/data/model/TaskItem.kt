2670f1cddf9ba79d03428d67d5df52cdaa35580f
twenk11k <selim_demiral@outlook.com>
2021-12-12 17:44:36 +0300

    Add PrimaryKey annotation to id in TaskItem

--- app/src/main/java/com/twenk11k/tasksearch/data/model/TaskItem.kt
+++ app/src/main/java/com/twenk11k/tasksearch/data/model/TaskItem.kt
@@ -2,12 +2,13 @@ package com.twenk11k.tasksearch.data.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity(tableName = "tasks")
 data class TaskItem(
-    val id: Int,
+    @PrimaryKey val id: Int,
     val name: String,
     val project: String,
     val status: Int
