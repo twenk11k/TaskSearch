040974cf2ff39f6c8b5f7c12085b70c6c5ffb6c7
twenk11k <selim_demiral@outlook.com>
2021-12-12 18:16:12 +0300

    Add BEARER const to Constants

--- app/src/main/java/com/twenk11k/tasksearch/util/Constants.kt
+++ app/src/main/java/com/twenk11k/tasksearch/util/Constants.kt
@@ -2,5 +2,6 @@ package com.twenk11k.tasksearch.util
 
 object Constants {
     const val BASE_URL = "https://www.meistertask.com/"
+    const val BEARER = "2N6edq_uS3ACq89RhzN2yQtdT5aEhbKgaE5-P9BD3hc"
     const val SEARCH_DELAY = 400L
 }
